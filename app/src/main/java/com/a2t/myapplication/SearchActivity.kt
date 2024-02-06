f15737b160d4ad75ccfc612d87fccdfd2a5288f7
A2T <luidor-arb@yandex.ru>
2024-02-06 15:44:02 +0400

    Домашнее задание PlaylistMaker v2.0

--- app/src/main/java/com/a2t/myapplication/SearchActivity.kt
+++ app/src/main/java/com/a2t/myapplication/SearchActivity.kt
@@ -1,11 +1,79 @@
 package com.a2t.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val arrow = findViewById<ImageView>(R.id.iv_arrow)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        if (savedInstanceState != null) {
+            inpurString = savedInstanceState.getString("INPUT_STRING", "")
+            searchEditText.setText(inpurString)
+        }
+
+        // Закрытие SearchActivity по стрелке
+        arrow.setOnClickListener {
+            finish()
+        }
+
+        // Нажатие на поле ввода
+        searchEditText.setOnClickListener {
+            it.requestFocus()
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(it, InputMethodManager.SHOW_IMPLICIT)
+        }
+
+        // Нажатие на кнопку очистить
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) {
+                    startSearch()
+                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                inpurString = searchEditText.text.toString()
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("INPUT_STRING", inpurString)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun startSearch () {
+        Toast.makeText(this@SearchActivity, getString(R.string.search_text), Toast.LENGTH_SHORT).show()
     }
 }
+
+private var inpurString: String = ""
--- app/src/main/java/com/a2t/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/a2t/myapplication/SettingsActivity.kt
@@ -1,16 +1,50 @@
 package com.a2t.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Locale
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val arrow = findViewById<ImageView>(R.id.iv_arrow)
+        val ll_send = findViewById<LinearLayout>(R.id.ll_send)
+        val ll_support = findViewById<LinearLayout>(R.id.ll_support)
+        val ll_user_agreement = findViewById<LinearLayout>(R.id.ll_user_agreement)
+
+        // Закрытие SettingsActivity по стрелке
         arrow.setOnClickListener {
             finish()
+        }
+
+        // Поделиться приложением
+        ll_send.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.practicum_android))
+            val chooserIntent = Intent.createChooser(intent, getString(R.string.mail))
+            startActivity(chooserIntent)
+        }
+
+        // Техподдержка
+        ll_support.setOnClickListener {
+            val email = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            email.putExtra(Intent.EXTRA_EMAIL, arrayOf<String>(getString(R.string.e_mail)))
+            email.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.message_subject))
+            email.putExtra(Intent.EXTRA_TEXT, getString(R.string.message_thank_you))
+            startActivity(Intent.createChooser(email, getString(R.string.mail)))
+        }
+
+        // Пользовательское соглашение
+        ll_user_agreement.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.practicum_offer)))
+            startActivity(browserIntent)
         }
     }
 }
