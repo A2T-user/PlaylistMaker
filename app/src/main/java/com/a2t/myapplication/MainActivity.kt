52c77fd5e4e1495ae323f350dd60247c9de6240f
A2T <luidor-arb@yandex.ru>
2024-03-20 15:22:36 +0400

    Домашнее задание PlaylistMaker v5.0

--- app/src/main/java/com/a2t/myapplication/MainActivity.kt
+++ app/src/main/java/com/a2t/myapplication/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.a2t.myapplication
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+
+lateinit var sharedPrefs: SharedPreferences
+
+const val PLAYLIST_MAKER_PREFERENCES = "playlist_maker_preferences"     // Имя файла для сохраняемых параметров
+const val SWITCHER_KEY = "switcher_key"                                 // Ключ для состояния themeSwitcher (- )
+const val SEARCH_HISTORY_KEY = "search_history"                             // Ключ для истории поиска
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Получаем экземпляр Shared Preferences
+        sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        // Устанавливаем тему приложения согласно сохраненных параметров
+        (applicationContext as App).switchTheme(sharedPrefs.getBoolean(SWITCHER_KEY, false))
+
         val btnSearch = findViewById<Button>(R.id.btn_search)
         val btnMedia = findViewById<Button>(R.id.btn_media)
         val btnSettings = findViewById<Button>(R.id.btn_settings)
+
         // Кнопка ПОИСК
         btnSearch.setOnClickListener {
             val intent = Intent(this@MainActivity, SearchActivity::class.java)
